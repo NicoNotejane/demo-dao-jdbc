4996318d2da41bf130e2eb9f1443f1a047b54cb5
Nicolas Notejane <niconotejane@gmail.com>
2019-01-28 17:13:16 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,18 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());  
+        
+        System.out.println("\n=== TEST 5: seller insert ===");
+        seller = sellerDao.FindById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+        
+        
+        
+        
+        
         } 
+	
     }
 	
